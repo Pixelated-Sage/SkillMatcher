f7b7773c1186a07d3e6110b2fc57e46bae221712
Pixelated-Sage <abhisheksharma.cse59@gmail.com>
2025-07-03 15:56:42 +0530

    front end alignment

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,24 @@
-// Home.jsx
+import { useNavigate } from "react-router-dom";
+
 export default function Home() {
-  return <h1 className="text-2xl font-bold text-center mt-10">Welcome to SkillMatcher</h1>;
+  const navigate = useNavigate();
+
+  return (
+    <section>
+      <div className="max-w-2xl mx-auto text-center p-6">
+        <h1 className="text-4xl font-bold mt-10 mb-4">
+          Welcome to Skill Matcher
+        </h1>
+        <p className="font-gray-300 mt-3 mb-4">
+          Find your next move with your current skills
+        </p>
+        <button
+          onClick={() => navigate("/input")}
+          className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-300"
+        >
+          Get Started
+        </button>
+      </div>
+    </section>
+  );
 }
