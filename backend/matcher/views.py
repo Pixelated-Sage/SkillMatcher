74b81bbe613a71e05d67e53e7eaedd4002435444
Pixelated-Sage <abhisheksharma.cse59@gmail.com>
2025-07-03 15:32:16 +0530

    dummy data updated

--- backend/matcher/views.py
+++ backend/matcher/views.py
@@ -1,18 +1,25 @@
+import json
+import os
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+#import the json data 
+SKILL_DATA_PATH = os.path.join(os.path.dirname(__file__), "skill_data.json")
+with open(SKILL_DATA_PATH, "r") as f:
+    skill_data = json.load(f)
 
 @api_view(['POST'])
 def match_skills(request):
     skills = request.data.get('skills',[])
+    result = {}
 
-    recommendation = {
-        "Python": ["Data Science", "Web Development", "Machine Learning"],
-        "C++" : ["Game Development", "Systems Programming", "Embedded Systems"],
-    }
-
-    matched = {}
     for skill in skills:
-        matched[skill] = recommendation.get(skill,[])
-
-    return Response({"matches": matched})
+        if skill in skill_data:
+            result[skill] = skill_data.get(skill,[])
+        else:
+            result[skill] = {
+                "careers":["No data"],
+                "courses":["No data"],
+                "teammates":["No data"],
+            }
+    return Response({"matches": result})
