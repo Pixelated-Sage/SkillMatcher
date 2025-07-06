5285cf9513895ed0dc3091e0c2c898adef41d881
Pixelated-Sage <abhisheksharma.cse59@gmail.com>
2025-07-06 09:32:03 +0530

    .gitignore and added .env

--- README.md
+++ README.md
@@ -1,8 +1,20 @@
-# SkillMatcher 🔎
-A tool that takes your current skills and recommends what to learn next, what to build, or who to team up with.
+# SkillMatcher 🚀
 
-## 🚀 Tech Stack
-- Frontend: React + Tailwind
-- Backend: Django + DRF
-- DB: SQLite/PostgreSQL
-- Logic: Pandas, NumPy
+SkillMatcher helps users find courses, career paths, and teammates based on their current skills.
+
+## 🔧 Tech Stack
+
+- **Frontend:** React + Tailwind (Vercel)
+- **Backend:** Django REST Framework (Railway)
+- **Database:** JSON (MVP)
+- **Deployment:** Vercel (frontend) + Railway (backend)
+
+## 🛠 How to Run Locally
+
+### Backend
+```bash
+cd backend
+python -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+python manage.py runserver
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-91k=4&0m@b(n2_6$zxszu*_b51n=@r2b_n)iz^5+-19z@)v=z7'
+
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG" , default = False , cast = bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=lambda v: [s.strip() for s in v.split(',')])
 
 
 # Application definition
